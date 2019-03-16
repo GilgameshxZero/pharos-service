694fd4a003187eeee704893bfeba0b33920e10c7
ed1d1a8d <tong.t.wang@gmail.com>
2019-03-16 16:34:35 -0400

    Setup django project

---
+++ pharos_service/messenger_bot/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MessengerBotConfig(AppConfig):
+    name = 'messenger_bot'
---
+++ pharos_service/messenger_bot/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
